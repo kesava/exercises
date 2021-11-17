890a729ded23a9bae1f00f48c0f5476552cd7e32
Kesava Mallela <kesava@gmail.com>
2021-11-16 20:55:02 -0800

    Create index.js

---
+++ once/index.js
@@ -0,0 +1,12 @@
+export function once(fn) {
+  const lookup = new Map();
+  return function() {
+    const key = `${fn}-${[...arguments].join(",")}`;
+    if (lookup.has(key)) {
+      return lookup.get(key);
+    } else {
+      lookup.set(key, fn.apply(null, arguments));
+      return lookup.get(key);
+    }
+  }
+}
