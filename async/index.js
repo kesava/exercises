38665c4f4ef947b9903a579c4231fec0b020f75b
Kesava Mallela <kesava@gmail.com>
2021-11-17 10:04:45 -0800

    Update index.js

--- async/index.js
+++ async/index.js
@@ -1,1 +1,30 @@
-
+export const async = {
+  sequence(tasks) {
+    return function(cbfn) {
+      if (!tasks.length) {
+        cbfn();
+      }
+       var index = 0;
+      function next(data) {
+        if (index < tasks.length) {
+          tasks[index++](function(err, result) {
+            if (err) {
+              cbfn(err);
+            } else {
+              next(result);
+            }
+          }, data);
+        } else {
+          cbfn(null, data);
+        }
+      }
+      
+      next();
+    }
+  },
+  parallel(promises) {
+    return function(err, data) {
+      console.log({ err, data });
+    }
+  }
+};
